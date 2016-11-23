2161aa535684d8cb54c51453c2d5b824aec34806
Simon <simon.conway@condaal.com>
2016-11-23 15:08:40 +0000

    WIP : Player names

--- app.rb
+++ app.rb
@@ -2,15 +2,21 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
-  get '/names' do
+  get '/' do
     erb(:index)
   end
 
-  post '/names' do
+  post '/name' do
     p params
-    @name = params[:name]
-    erb(:index)
+    @player1 = params[:player1]
+    @player2 = params[:player2]
+    erb(:play)
   end
+
+  get '/play' do
+    erb(:play)
+  end
+
 
   run! if app_file == $0
 
