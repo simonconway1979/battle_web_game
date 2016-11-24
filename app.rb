f99f88424998b01d9241e7b30fde8b6e17d44f81
ajbater <amanda22@hotmail.co.uk>
2016-11-24 18:25:08 +0000

    Ex 25 - refactoring

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 require './lib/player.rb'
+require './lib/game.rb'
+
 
 class Battle < Sinatra::Base
 
@@ -23,6 +25,7 @@ class Battle < Sinatra::Base
   end
 
   get '/attack_successful' do
+    $player_1.game.attack($player_2)
     erb(:attack_successful)
     # redirect '/play'
   end
